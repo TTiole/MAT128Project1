4bf9c0d62e57b36e7a994e560db8b761b085c151
iole <eloi@gonzalezurbano.com>
2020-02-16 12:56:44 -0800

    Finished part ii

--- main.m
+++ main.m
@@ -1,2 +1,22 @@
+%% Part i
 phi = @(z) z^2 - 1.25;
+
+figure;
 FilledJulia(phi, [(1+sqrt(6))/2, (1-sqrt(6))/2], -1.8, 1.8, -0.7, 0.7, 0.01);
+
+%% Part ii
+c = 0.36 + 0.1*1i;
+phi = @(z) z^2 + c;
+fxdpnts = roots([1, -1, c])';
+
+figure;
+FilledJulia(phi, fxdpnts, -1.8, 1.8, -0.7, 0.7, 0.01);
+
+
+
+c = -0.123 - 0.745*1i;
+phi = @(z) z^2 + c;
+fxdpnts = roots([1, -1, c])';
+
+figure;
+FilledJulia(phi, fxdpnts, -1.8, 1.8, -0.7, 0.7, 0.01);
