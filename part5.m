148d104f68e0243c324ea3c7409cc931203b28c3
yqshen1998 <46582115+yqshen1998@users.noreply.github.com>
2020-02-18 14:09:33 -0800

    fix bugs

--- part5.m
+++ part5.m
@@ -28,7 +28,7 @@ function result = connected (phi, ar, br, ai, bi, step,c)
     end
     result = 0; %the set is connected if result is 0
     for k = 1: length(orbit)
-        if ismember(0, orbit(i)) == 0
+        if ismember(O, orbit(i)) == 0
             result = result+1;
         end
     end           
