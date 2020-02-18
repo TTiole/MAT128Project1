ec6914d1773ce9e9468de54776e72f2c99ca7809
iole <eloi@gonzalezurbano.com>
2020-02-18 08:47:54 -0800

    Added figures from parts i-iii. Modified main.m to include a few more plots for a more thorough report of part ii

--- main.m
+++ main.m
@@ -4,6 +4,12 @@ phi = @(z) z^2 - 1.25;
 figure;
 FilledJulia(phi, [(1+sqrt(6))/2, (1-sqrt(6))/2], -1.8, 1.8, -0.7, 0.7, 0.01);
 
+phi = @(z) z^2;
+
+figure
+FilledJulia(phi, [(1+sqrt(6))/2, (1-sqrt(6))/2], -1, 1, -1, 1, 0.01);
+pbaspect([1 1 1])
+
 %% Part ii
 c = 0.36 + 0.1*1i;
 phi = @(z) z^2 + c;
@@ -12,6 +18,8 @@ fxdpnts = roots([1, -1, c])';
 figure;
 FilledJulia(phi, fxdpnts, -1.8, 1.8, -0.7, 0.7, 0.01);
 
+figure;
+FilledJulia(phi, fxdpnts, 0, 2, -2, 0, 0.01);
 
 
 c = -0.123 - 0.745*1i;
@@ -20,6 +28,16 @@ fxdpnts = roots([1, -1, c])';
 
 figure;
 FilledJulia(phi, fxdpnts, -1.8, 1.8, -0.7, 0.7, 0.01);
+
+figure;
+hold on
+FilledJulia(phi, fxdpnts, -2, 2, -2, 2, 0.01);
+syms x y
+fimplicit((x).^2 + (y).^2 -4, '--')
+fimplicit((x).^2 + (y).^2 -1, '--')
+axis equal
+hold off
+
 
 %% Part iii
 ar = -1.8; br = 1.8; ai = -0.7; bi = 0.7; step = 0.01;
