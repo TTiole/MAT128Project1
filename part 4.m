556eb8d7b93531a197bcf37399823a070f222b33
yqshen1998 <46582115+yqshen1998@users.noreply.github.com>
2020-02-18 11:40:16 -0800

    add comments

--- part 4.m
+++ part 4.m
@@ -3,14 +3,14 @@ dim = cal_dimension(image);
 
 function dim = cal_dimension(image)
      figure = double(image);
-     m = size(figure,1);
+     m = size(figure,1); %create grid
      a = floor(log2(m));
      b = 2.^(1:a);
      Nr = zeros(1,a);
      for i = 1:a
          c = b(i);
          d = m/c;
-         for j = 1:d
+         for j = 1:d %differential box-counting method
              for k = 1:d
                  A = figure(c*(j-1)+[1:c],c*(k-1)+[1:c]);
                  minim = min(A(1:end));
@@ -21,6 +21,6 @@ function dim = cal_dimension(image)
          end
      end
      r = m./b;
-     val = polyfit(log10(r),log10(Nr),1);
+     val = polyfit(log10(r),log10(Nr),1); %least square linear fit
      dim = val(1);
 end
